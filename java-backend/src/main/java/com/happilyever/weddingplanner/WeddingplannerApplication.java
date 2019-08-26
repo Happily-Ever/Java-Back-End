dd00c7b01114f37c62103c201a4d10384162312a
KimberlySwinton <kimberlyswinton@gmail.com>
2019-08-26 16:54:45 -0400

    :tada: initial commit
    initial commit

---
+++ java-backend/src/main/java/com/happilyever/weddingplanner/WeddingplannerApplication.java
@@ -0,0 +1,15 @@
+package com.happilyever.weddingplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingplannerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(WeddingplannerApplication.class, args);
+    }
+
+}
