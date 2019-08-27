a481bdc6d30a31d447584c31287503272fd5b884
KimberlySwinton <kimberlyswinton@gmail.com>
2019-08-26 21:15:36 -0400

    :construction: added config classes working on models
    added config classes working on models

---
+++ java-backend/src/main/java/com/happilyever/weddingplanner/models/User.java
@@ -0,0 +1,33 @@
+package com.happilyever.weddingplanner.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+// User is considered the parent entity
+
+@Entity
+@Table(name = "users")
+public class User extends Auditable
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long userid;
+
+    @Column(nullable = false,
+            unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    @Column(nullable = false,
+            unique = true)
+    private String email;
+
