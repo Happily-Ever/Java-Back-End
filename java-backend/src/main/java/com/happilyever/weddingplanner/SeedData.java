b525af52bab38f6329f5d3513092fe3cea8844d8
KimberlySwinton <kimberlyswinton@gmail.com>
2019-08-29 13:39:22 -0400

    :bug: trying to deploy
    trying to deploy

--- java-backend/src/main/java/com/happilyever/weddingplanner/SeedData.java
+++ java-backend/src/main/java/com/happilyever/weddingplanner/SeedData.java
@@ -1,32 +1,32 @@
-package com.happilyever.weddingplanner;
-
-import com.happilyever.weddingplanner.models.Wedding;
-import com.happilyever.weddingplanner.models.User;
-import com.happilyever.weddingplanner.services.WeddingService;
-import com.happilyever.weddingplanner.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-
-@Transactional
-@Component
-public class SeedData implements CommandLineRunner
-{
-    @Autowired
-    WeddingService weddingService;
-
-    @Autowired
-    UserService userService;
-
-
-    @Override
-    public void run(String[] args) throws Exception
-    {
-        ArrayList<Wedding> wedding = new ArrayList<>();
-        User u1 = new User("admin", "password", "admin@email.local", "location", wedding);
-        userService.save(u1);
-    }
-}
+//package com.happilyever.weddingplanner;
+//
+//import com.happilyever.weddingplanner.models.Wedding;
+//import com.happilyever.weddingplanner.models.User;
+//import com.happilyever.weddingplanner.services.WeddingService;
+//import com.happilyever.weddingplanner.services.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.ArrayList;
+//
+//@Transactional
+//@Component
+//public class SeedData implements CommandLineRunner
+//{
+//    @Autowired
+//    WeddingService weddingService;
+//
+//    @Autowired
+//    UserService userService;
+//
+//
+//    @Override
+//    public void run(String[] args) throws Exception
+//    {
+//        ArrayList<Wedding> wedding = new ArrayList<>();
+//        User u1 = new User("admin", "password", "admin@email.local", "location", wedding);
+//        userService.save(u1);
+//    }
+//}
--- java-backend/src/main/java/com/happilyever/weddingplanner/config/AuthorizationServerConfig.java
+++ java-backend/src/main/java/com/happilyever/weddingplanner/config/AuthorizationServerConfig.java
@@ -14,8 +14,8 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 @EnableAuthorizationServer
 class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter
 {
-    //    static final String CLIENT_ID = System.getenv("OAUTHCLIENTID"); // read from environment variable
-    //    static final String CLIENT_SECRET = System.getenv("OAUTHCLIENTSECRET"); // read from environment variable
+//        static final String CLIENT_ID = System.getenv("OAUTHCLIENTID"); // read from environment variable
+//        static final String CLIENT_SECRET = System.getenv("OAUTHCLIENTSECRET"); // read from environment variable
     static final String CLIENT_ID = "lambda-client";
     static final String CLIENT_SECRET = "lambda-secret";
     static final String GRANT_TYPE_PASSWORD = "password";
